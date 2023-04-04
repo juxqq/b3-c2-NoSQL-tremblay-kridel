c1e69ab22b0c85bdc7f705e77235c0daefcc0f42
juxqq <75832820+juxqq@users.noreply.github.com>
2023-04-04 17:45:23 +0200

    commit nassim

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,28 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Ecole(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    nom = models.CharField(max_length=100)
+    adresse = models.CharField(max_length=250)
+    ville = models.CharField(max_length=50)
+    codePostal = models.CharField(max_length=5)
+
+class Reservation(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ecole = models.ForeignKey(Ecole, on_delete=models.CASCADE)
+    date = models.DateTimeField(null=False, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, db_index=True)
+
--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form, 'title': 'Login'})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form, 'title': 'Create Account'})
+
+def sign_out(request):
+    logout(request)
+    return redirect('/reservations/login')
